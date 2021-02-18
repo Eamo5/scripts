03915cf221648e7357e5d245485033fdb0eb5d71
Eamon Powell <eamonpowell@outlook.com>
2021-02-18 17:15:17 +1100

    makerom.sh: improve

--- makerom.sh
+++ makerom.sh
@@ -1,60 +1,58 @@
 #!/bin/bash
-# ./makerom.sh <rom> <device> <optional sync> <optional discard>
-# eg. ./makerom.sh lineage17 taimen sync
+# ./makerom.sh <rom> <device> <optional sync> <optional pick> <optional upload>
+# eg. ./makerom.sh lineage18 taimen sync
 
 ## Variables
-ROM_DIR=""$HOME"/roms/"$1""
+if [ -e "$HOME"/roms/"$1" ]; then
+	ROM_DIR=""$HOME"/roms/"$1""
+else
+	echo "Unable to find rom directory. YOLO try and build anyway."
+	ROM_DIR="$pwd"
+fi
+
+case "$2" in
+	coral)
+		PARENT=13htD7BIUPErNCbq8KUZsZVdWho5MMk0x ;;
+	flame)
+		PARENT=1JJbsXJLw_3A5AL_AxC0Q4nty3lFmZyan ;;
+	taimen)
+		PARENT=1bt9WnzQ4LZlgzRgUaEAqaXJfEvNj2Nug ;;
+	walleye)
+		PARENT=1gCPZ289x_kgsvp2WJyXacRNY7hCg3r8J ;;
+esac
 
 ## Environment
 set -e
 cd "$ROM_DIR"
 source "$ROM_DIR/build/envsetup.sh"
 
-# when signing builds, /tmp being too small can be problematic
-sudo mount -o remount,size=15G /tmp
-
 ## Sync
-if [ "$3" == "sync" ] && [ "$4" == "" ]; then
-	repo sync -c --force-sync --no-clone-bundle --no-tags -j8
-elif [ "$3" == "sync" ] && [ "$4" == "discard" ]; then
-        repo sync -c -d --force-sync --no-clone-bundle --no-tags -j8
+if [ "$3" == "sync" ] || [ "$4" == "sync" ] || [ "$5" == "sync" ]; then
+	repo sync -c --force-sync --no-clone-bundle --no-tags -j8 -d
+fi
+
+if [ "$3" == "patch" ] || [ "$4" == "patch" ] || [ "$5" == "patch" ]; then
+	bash "$ROM_DIR"/patch.sh
 fi
 
 ## Build
 echo "Starting build..."
 case "$1" in
-	lineage17)
+	lineage18 | lineage17)
 		breakfast lineage_"$2"-userdebug
 		m installclean
 		brunch lineage_"$2"-userdebug ;;
-	aosip)
-		lunch aosip_"$2"-user
+	*)
+		breakfast "$1"_"$2"-userdebug
 		m installclean
-		m kronic ;;
-	aicp)
-		breakfast aicp_"$2"-userdebug
-		m installclean
-		brunch aicp_"$2"-userdebug ;;
+		brunch "$1"_"$2"-userdebug ;;
 esac
 
-## Build Signing
-if [ "$1" == "lineage17" ]; then
-	m target-files-package otatools
-
-	# Sign APKs
-	echo "Signing APKs..."
-	./build/tools/releasetools/sign_target_files_apks -o -d "$HOME"/.android-certs \
-	    $OUT/obj/PACKAGING/target_files_intermediates/*-target_files-*.zip \
-	    signed-target_files.zip
-
-	# Generate Install Package
-	./build/tools/releasetools/ota_from_target_files -k "$HOME"/.android-certs/releasekey \
-	    --block \
-	    signed-target_files.zip \
-	    signed-ota_update.zip
-
-	# Move target package to $OUT
-	mv signed-ota_update.zip $OUT/lineage-17.1-UNOFFICIAL-"$2".zip
+if [ "$3" == "upload" ] || [ "$4" == "upload" ] || [ "$5" == "upload" ] && [ "$1" == "lineage18" ]; then
+	gdrive upload -p "$PARENT" "$OUT"/lineage-18.1-*.zip
+	if [ "$2" == "coral" ] || [ "$2" == "flame" ]; then
+		gdrive upload -p "$PARENT" "$OUT"/boot.img
+	fi
 fi
 
 echo "Done!"
